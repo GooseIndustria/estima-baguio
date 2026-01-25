b9bc45de2464ce714f009b1f0d1520f981e95eeb
GooseIndustria <goose.industria@gmail.com>
2026-01-25 23:37:56 +0800

    fix(build): Increase PWA cache limit for high-res assets

--- app/vite.config.js
+++ app/vite.config.js
@@ -40,6 +40,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
         runtimeCaching: [
           {
